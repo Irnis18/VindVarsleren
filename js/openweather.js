16721e19ef4ed9eed0b3c49dda482527f7359dd4
IRNIS BESIROVICqadgit configgit config --global user.name IRNIS <irnis99@hotmail.com>
2016-10-11 11:12:39 +0200

    initial commit

--- js/openweather.js
+++ js/openweather.js
@@ -1,32 +1,46 @@
-var weather;
-var api = "http://api.openweathermap.org/data/2.5/weather?q=";
-var apiKey="&APPID=569cb5c54488af8f488e82b8dd9ca105";
-var units= "&units=metric";
-var input;
-$(document).ready(function(){
+function weatherAsk(event){
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily";
+  var apiKey="569cb5c54488af8f488e82b8dd9ca105";
+  var units= "metric";
+  var cnt = 7;
+  event.preventDefault();
 
+  parameters = {
+    q: $("#city").val(),
+    APPID: apiKey,
+    units: units,
+    cnt: 7
+  };
+
+  $.getJSON(apiUrl, parameters, function(data){
+
+    owmTodayDataWeatherIcon = $("#owm-today-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + ".png");
+
+    owmTomorrowDataWeatherIcon = $("#owm-tomorrow-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[1].weather[0].icon + ".png");
+
+    owmInSevenDaysDataWeatherIcon = $("#owm-seven-days-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[6].weather[0].icon + ".png");
+
+
+    todayWeatherString = "The weather is " + data.list[0].weather[0].main + "<br/>" +"Average temperature is: " + data.list[0].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[0].pressure + " hpa"  + "<br/>" + "Cloudiness: " + data.list[0].weather[0].description;
+
+    tomorrowWeatherString = "The weather is " + data.list[1].weather[0].main + "<br/>" + "Average temperature is: " + data.list[1].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[1].pressure + " hpa" +  "<br/>" + "Cloudiness: " + data.list[1].weather[0].description;
+
+    inSevenDaysWeather = "The weather is " + data.list[6].weather[0].main + "<br/>" + "Average temperature is: " + data.list[6].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[6].pressure + " hpa" + "<br/>" +"Cloudiness: " + data.list[6].weather[0].description;
+
+    $("#owm-today-weather > .text").html(todayWeatherString);
+    $("#owm-tomorrow-weather > .text").html(tomorrowWeatherString);
+    $("#owm-seven-days-weather > .text").html(inSevenDaysWeather);
+  });
+}
+
+
+
+$(document).ready(function(){
+  $(".typahead").submit(weatherAsk);
 });
 
 
-// $(document).ready(function(){
-//
-// var weather;
-// var api = "http://api.openweathermap.org/data/2.5/weather?q=";
-// var apiKey="&APPID=569cb5c54488af8f488e82b8dd9ca105";
-// var units="&units=metric";
-// var input;
-//
-//  input = $("#city");
-//
-//  function weatherAsk(event){
-//  event.preventDefault();
-//  var url = api + input.value() + apiKey + units;
-//  }
-//
-// function gotData(data) {
-//   weather = data;
-//   $.getJSON(url, gotData);
-// }
+
 //
 // $('owmTodayWeather').first().html(weather.main.temp);
 // $(".typahead").submit(weatherAsk);
