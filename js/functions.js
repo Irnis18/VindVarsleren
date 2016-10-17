0ab24fe11633a81cb53ccea0455642a8a1cd4175
IRNIS BESIROVICqadgit configgit config --global user.name IRNIS <irnis99@hotmail.com>
2016-10-17 14:05:25 +0200

    initial commit

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 /*********************************************************************************
-Api-Openweather - This is the function for the openweathermap api.
+Api-Openweather - This is the function for the openweathermap api. using JSON
 **********************************************************************************/
+
 function weatherAsk(event){
   var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily";
   var apiKey="569cb5c54488af8f488e82b8dd9ca105";
@@ -17,33 +18,39 @@ function weatherAsk(event){
 
   $.getJSON(apiUrl, parameters, function(data){
 
+//The icon for Todays weather, Tomorrows weather, and the weahter in seven days
     owmTodayDataWeatherIcon = $("#owm-today-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + ".png");
-
     owmTomorrowDataWeatherIcon = $("#owm-tomorrow-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[1].weather[0].icon + ".png");
-
     owmInSevenDaysDataWeatherIcon = $("#owm-seven-days-weather > .weather-icon-data").attr("src", 'http://openweathermap.org/img/w/' + data.list[6].weather[0].icon + ".png");
 
 
+// What is going to be displayed in div, todays weather, tomorrows weather and the weather in seven days.
     todayWeatherString = "The weather is " + data.list[0].weather[0].main + "<br/>" +"Average temperature is: " + data.list[0].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[0].pressure + " hpa"  + "<br/>" + "Cloudiness: " + data.list[0].weather[0].description;
-
     tomorrowWeatherString = "The weather is " + data.list[1].weather[0].main + "<br/>" + "Average temperature is: " + data.list[1].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[1].pressure + " hpa" +  "<br/>" + "Cloudiness: " + data.list[1].weather[0].description;
-
     inSevenDaysWeather = "The weather is " + data.list[6].weather[0].main + "<br/>" + "Average temperature is: " + data.list[6].temp.day + "°C" + "<br/>" + "Pressure: " + data.list[6].pressure + " hpa" + "<br/>" +"Cloudiness: " + data.list[6].weather[0].description;
 
+
+//Displaying the icon and the text using jquerry
     $("#owm-today-weather > .text").html(todayWeatherString);
     $("#owm-tomorrow-weather > .text").html(tomorrowWeatherString);
     $("#owm-seven-days-weather > .text").html(inSevenDaysWeather);
+
+
   });
 }
-
+// Running the function that we made when the document is fully lodaded.
 $(document).ready(function(){
   $(".typahead").submit(weatherAsk);
 
 });
 
+
+
 /*********************************************************************************
 This is the function for the autocomplete typahead.
 **********************************************************************************/
+
+//making a fucntion for the typahead, autocomplete.
 $(function ()
  {
      $("#city").autocomplete({
@@ -65,3 +72,5 @@ $(function ()
      });
      $("#city").autocomplete("option", "delay", 100);
     });
+    xhr.open("GET", "index.html");
+    xhr.send();
